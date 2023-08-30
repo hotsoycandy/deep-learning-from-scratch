b49a8f5b896b2e200b5ba810a8851e30d449d635
hotsoycandy <wkdwnsghd617@gmail.com>
2023-08-30 17:20:22 +0900

    렐루 활성화 함수 그래프 추가

--- activations/relu.py
+++ activations/relu.py
@@ -3,3 +3,12 @@ import matplotlib.pylab as plt
 
 def relu (x) :
   return np.maximum(0, x)
+
+if __name__ == '__main__' :
+  x = np.arange(-5, 5, 0.1)
+  y = relu(x)
+
+  plt.plot(x, y)
+  plt.xlim(-5.1, 5.1)
+  plt.ylim(-0.1, 5.1)
+  plt.show()
