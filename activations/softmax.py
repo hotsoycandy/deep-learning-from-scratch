1a5110a993c0495e5391628d05fda63e44cdbff3
hotsoycandy <wkdwnsghd617@gmail.com>
2023-08-30 17:21:30 +0900

    소프트맥스 활성화 함수 그래프 추가

--- activations/softmax.py
+++ activations/softmax.py
@@ -5,3 +5,10 @@ def softmax (x) :
   exp = np.exp(x - np.max(x))
   exp_num = np.sum(exp)
   return exp / exp_num
+
+if __name__ == '__main__' :
+  x = np.array([4, 7, 6, 3])
+  y = softmax(x)
+
+  plt.bar(x, y)
+  plt.show()
