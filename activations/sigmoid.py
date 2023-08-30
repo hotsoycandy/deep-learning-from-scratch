78a6432ef42952fc9be77a5d08023bed8d0495a5
hotsoycandy <wkdwnsghd617@gmail.com>
2023-08-30 16:46:54 +0900

    시그모이드 활성화 함수 그래프 추가

--- activations/sigmoid.py
+++ activations/sigmoid.py
@@ -3,3 +3,12 @@ import matplotlib.pylab as plt
 
 def sigmoid (x) :
   return 1 / (1 + np.exp(-x))
+
+if __name__ == '__main__' :
+  x = np.arange(-5, 5, 0.1)
+  y = sigmoid(x)
+
+  plt.plot(x, y)
+  plt.xlim(-5.1, 5.1)
+  plt.ylim(-0.1, 1.1)
+  plt.show()
