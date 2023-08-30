5bfcc12dcaa71b8ac1e1b6d7434175a6de8f20cc
hotsoycandy <wkdwnsghd617@gmail.com>
2023-08-30 16:45:54 +0900

    계단 함수 그래프 표시 코드 추가

--- activations/step.py
+++ activations/step.py
@@ -5,3 +5,10 @@ def step (x) :
   y = x > 0
   print(y.astype(np.int32))
   return y.astype(np.int32)
+
+if __name__ == '__main__' :
+  x = np.arange(-5, 5, 0.1)
+  y = step(x)
+
+  plt.plot(x, y)
+  plt.show()
