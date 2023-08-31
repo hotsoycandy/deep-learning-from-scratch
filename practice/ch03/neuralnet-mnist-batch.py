42438f587882a0e4c23f35ec92d9edef018c56d5
hotsoycandy <wkdwnsghd617@gmail.com>
2023-08-31 09:20:22 +0900

    Create neuralnet-mnist-batch.py

---
+++ practice/ch03/neuralnet-mnist-batch.py
@@ -0,0 +1,63 @@
+# configure to import internal modules
+import sys, os
+sys.path.append(
+  os.path.abspath(
+    os.path.join(
+      os.path.dirname(__file__),
+      '../../'
+    )))
+
+import pickle
+import numpy as np
+from activations.sigmoid import sigmoid
+from activations.softmax import softmax
+from PIL import Image
+from dataset.mnist import load_mnist
+
+def get_data () :
+  (x_train, t_train), (x_test, t_test) \
+    = load_mnist(
+      flatten = True,
+      normalize = True,
+      one_hot_label = False
+    )
+  return x_test, t_test
+
+def init_network () :
+  filePath = os.path.abspath(
+    os.path.join(
+      os.path.dirname(__file__),
+      'sample_weight.pkl'
+    ))
+
+  with open(filePath, 'rb') as f :
+    network = pickle.load(f)
+
+  return network
+
+def predict (network, x) :
+  W1, W2, W3 = network['W1'], network['W2'], network['W3']
+  b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+  a1 = np.dot(x, W1) + b1
+  z1 = sigmoid(a1)
+  a2 = np.dot(z1, W2) + b2
+  z2 = sigmoid(a2)
+  a3 = np.dot(z2, W3) + b3
+  y = softmax(a3)
+
+  return y
+
+if __name__ == '__main__' :
+  x, t = get_data()
+  network = init_network()
+
+  batch_size = 100
+  accuracy_cnt = 0
+
+  for i in range(0, len(x), batch_size) :
+    y = predict(network, x[i : i + batch_size])
+    p = np.argmax(y, axis = 1)
+    accuracy_cnt += np.sum(p == t[i : i + batch_size])
+
+  print(accuracy_cnt)
