e5461fccd2a7bd5231156f21702428e1b010b045
hotsoycandy <wkdwnsghd617@gmail.com>
2023-08-31 21:56:23 +0900

    Create mini_batch_neuralnet.py

---
+++ practice/ch04/mini_batch_neuralnet.py
@@ -0,0 +1,45 @@
+import sys, os
+sys.path.append(
+  os.path.abspath(
+    os.path.join(
+      os.path.dirname(__file__),
+      '../../'
+    )))
+
+import numpy as np
+from matplotlib.pylab import plt
+from dataset.mnist import load_mnist
+from TwoLayerNet import TwoLayerNet
+
+(x_train, t_train), (x_test, t_test) \
+  = load_mnist(
+    normalize = True,
+    one_hot_label = True
+  )
+
+train_loss_list = []
+
+# 하이퍼파라메터
+iter_sum = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_late = 0.1
+
+net = TwoLayerNet(input_size = 784, hidden_size = 50, output_size = 10)
+
+for i in range(iter_sum) :
+  print('epoch: ', i)
+  batch_mask = np.random.choice(train_size, batch_size)
+  x_batch = x_train[batch_mask]
+  t_batch = t_train[batch_mask]
+
+  grad = net.numerical_gradient(x_batch, t_batch)
+
+  for key in ('W1', 'b1', 'W2', 'b2') :
+    net.params[key] -= learning_late * grad[key]
+
+  loss = net.loss(x_batch, t_batch)
+  train_loss_list.append(loss)
+
+plt.plot(train_loss_list)
+plt.show()
