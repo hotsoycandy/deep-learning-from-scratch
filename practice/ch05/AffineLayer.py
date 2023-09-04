f3222b311ab9278ad46efc83108ccb0e423e94d3
hotsoycandy <wkdwnsghd617@gmail.com>
2023-09-04 15:43:16 +0900

    Create AffineLayer.py

---
+++ practice/ch05/AffineLayer.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+class AffineLayer :
+  def __init__ (self, W, b) :
+    self.W = W
+    self.b = b
+    self.x = None
+    self.dW = None
+    self.db = None
+
+  def forward (self, x) :
+    self.x = x
+    return np.dot(x, self.W) + self.b
+
+  def backward (self, dOut) :
+    dx = np.dot(dOut, self.W.T)
+    self.dW = np.dot(self.x.T, dOut)
+    self.db = np.sum(dOut, axis = 0)
+    return dx
